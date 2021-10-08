5f8a14f8840809df4a92c7afe31939e0f53a0a8f
Yago <nietogarridoyago@gmail.com>
2021-10-08 14:24:11 +0200

    residencia y SO

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba
 	public static void main(String args[])
 	{
 	System.out.print("yago nieto garrido");
-	System.out.print("Nacido en León");
+	System.out.print("Nacido en León en 2002");
+	System.out.print("Ahora vive en león y usa Windows 10");
 	}
 }
